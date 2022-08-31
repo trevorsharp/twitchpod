00a1f01cecac1ab4c8e9947eac38160188fd0a95
Trevor Sharp <sharp.trevor@me.com>
2022-08-30 19:45:51 -0700

    Refactor

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -27,9 +27,7 @@ const MainPage = ({ user, errorMessage, host }: MainPageProps) => {
     defaultValues: { searchText: user?.username },
   });
 
-  useEffect(() => {
-    setFocus('searchText');
-  }, []);
+  useEffect(() => setFocus('searchText'), []);
 
   const onSubmit = handleSubmit((values) => {
     if (values.searchText) {
@@ -42,52 +40,46 @@ const MainPage = ({ user, errorMessage, host }: MainPageProps) => {
     <>
       <Head>
         <title>twitchPOD</title>
-        <meta name="description" content="Create podcast feeds from Twitch VODs" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="112x112" href="/apple-touch-icon.png" />
       </Head>
-
-      <main className="h-full min-h-fit bg-white text-neutral-800 dark:bg-neutral-900 dark:text-white">
-        <div className="flex h-full min-h-fit flex-col items-center justify-center gap-16 p-8 text-center">
-          <h1 className="flex flex-col text-7xl font-bold">
-            <span>Welcome to</span>
-            <span className="text-twitch">
-              <Link href="/">twitchPOD</Link>
-            </span>
-          </h1>
-          <p className="flex flex-col text-2xl">
-            <span>Generate podcast feeds for your</span>
-            <span>
-              favorite <span className="text-twitch">Twitch</span> streamers
-            </span>
-          </p>
-          <form className="flex items-center gap-4" onSubmit={onSubmit}>
-            <SearchInput {...register('searchText')} />
-            <button type="submit">
-              <img
-                className="h-8 w-8 text-twitch"
-                src={isLoading ? '/loading.svg' : '/next.svg'}
-                alt="Submit"
-              />
-            </button>
-          </form>
-          {user && (
-            <div className="flex flex-col items-center gap-6">
-              <a className="flex items-center gap-4" target="_new" href={user.url}>
-                <img className="h-16 w-16 rounded-full" src={user.profileImageUrl} alt="Profile" />
-                <p className="text-4xl font-bold">{user.displayName}</p>
-              </a>
-              <QualitySelection selection={qualitySelection} onSelect={setQualitySelection} />
-              <RssLinks
-                host={host ?? window.location.host}
-                username={user.username}
-                quality={qualitySelection}
-              />
-            </div>
-          )}
-          {errorMessage && <p>{errorMessage}</p>}
-        </div>
-      </main>
+      <div className="flex h-full min-h-fit flex-col items-center justify-center gap-16 p-8 text-center">
+        <h1 className="flex flex-col text-7xl font-bold">
+          <span>Welcome to</span>
+          <span className="text-twitch">
+            <Link href="/">twitchPOD</Link>
+          </span>
+        </h1>
+        <p className="flex flex-col text-2xl">
+          <span>Generate podcast feeds for your</span>
+          <span>
+            favorite <span className="text-twitch">Twitch</span> streamers
+          </span>
+        </p>
+        <form className="flex items-center gap-4" onSubmit={onSubmit}>
+          <SearchInput {...register('searchText')} />
+          <button type="submit">
+            <img
+              className="h-8 w-8 text-twitch"
+              src={isLoading ? '/loading.svg' : '/next.svg'}
+              alt="Submit"
+            />
+          </button>
+        </form>
+        {user && (
+          <div className="flex flex-col items-center gap-6">
+            <a className="flex items-center gap-4" target="_new" href={user.url}>
+              <img className="h-16 w-16 rounded-full" src={user.profileImageUrl} alt="Profile" />
+              <p className="text-4xl font-bold">{user.displayName}</p>
+            </a>
+            <QualitySelection selection={qualitySelection} onSelect={setQualitySelection} />
+            <RssLinks
+              host={host ?? window.location.host}
+              username={user.username}
+              quality={qualitySelection}
+            />
+          </div>
+        )}
+        {errorMessage && <p>{errorMessage}</p>}
+      </div>
     </>
   );
 };
--- src/components/RssLinks.tsx
+++ src/components/RssLinks.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react';
 import { Quality } from '../types';
 
-const RssLinks = ({
-  host,
-  username,
-  quality,
-}: {
+type RssLinksProps = {
   host: string;
   username: string;
   quality: Quality;
-}) => {
+};
+
+const RssLinks = ({ host, username, quality }: RssLinksProps) => {
   const [copiedText, setCopiedText] = useState<string>('');
 
   const getRssLink = () =>
@@ -25,12 +23,12 @@ const RssLinks = ({
     <div className="flex h-24 flex-col items-center gap-6">
       <div className="flex gap-4">
         <a href={`podcast://${getRssLink()}`}>
-          <img className="h-10 w-10" src="/applepodcasts.svg" alt="apple podcasts" />
+          <img className="h-10 w-10" src="/applepodcasts.svg" alt="Apple Podcasts" />
         </a>
         <a href={`pktc://subscribe/${getRssLink()}`}>
-          <img className="h-10 w-10" src="/pocketcasts.svg" alt="pocket casts" />
+          <img className="h-10 w-10" src="/pocketcasts.svg" alt="Pocket Casts" />
         </a>
-        <img className="h-10 w-10 cursor-pointer" src="/rss.svg" alt="rss" onClick={copyRssLink} />
+        <img className="h-10 w-10 cursor-pointer" src="/rss.svg" alt="RSS" onClick={copyRssLink} />
       </div>
       {copiedText && <p>{copiedText}</p>}
     </div>
