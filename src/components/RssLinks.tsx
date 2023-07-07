d48847738d58c3323ebe9abb207fc82f554c50bc
Trevor Sharp <sharp.trevor@me.com>
2023-07-07 14:55:18 +0000

    Disable apple podcasts for video / add tooltip

--- src/components/RssLinks.tsx
+++ src/components/RssLinks.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Tooltip } from 'react-tooltip';
 import { Quality } from '../types';
 
 type RssLinksProps = {
@@ -22,15 +23,28 @@ const RssLinks = ({ host, username, quality }: RssLinksProps) => {
   return (
     <div className="flex h-24 flex-col items-center gap-6">
       <div className="flex gap-4">
-        <a href={`podcast://${getRssLink()}`}>
-          <img className="h-10 w-10" src="/applepodcasts.svg" alt="Apple Podcasts" />
-        </a>
+        {quality === Quality.Audio ? (
+          <a href={`podcast://${getRssLink()}`}>
+            <img className="h-10 w-10" src="/applepodcasts.svg" alt="Apple Podcasts" />
+          </a>
+        ) : (
+          <>
+            <img
+              data-tooltip-id="tooltip"
+              data-tooltip-html="<p>Apple Podcasts does not support twitchPOD's video format.</p><p>Try using Pocket Casts or addding the feed as 'Audio Only'.</p>"
+              className="h-10 w-10"
+              src="/applepodcasts-disabled.svg"
+              alt="Apple Podcasts"
+            />
+          </>
+        )}
         <a href={`pktc://subscribe/${getRssLink()}`}>
           <img className="h-10 w-10" src="/pocketcasts.svg" alt="Pocket Casts" />
         </a>
         <img className="h-10 w-10 cursor-pointer" src="/rss.svg" alt="RSS" onClick={copyRssLink} />
       </div>
       {copiedText && <p>{copiedText}</p>}
+      <Tooltip id="tooltip" />
     </div>
   );
 };
