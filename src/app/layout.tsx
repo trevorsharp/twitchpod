6527c12ac31aac8b7bcc8d1a8fdb7cec88a22856
Trevor Sharp <sharp.trevor@me.com>
2023-11-17 14:54:02 +0000

    Update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ export const runtime = 'edge';
 export const metadata: Metadata = {
   title: 'twitchPOD',
   description: 'Generate podcast feeds for your favorite Twitch streamers',
+  icons: { icon: { rel: 'icon', url: '/favicon.ico' }, apple: { rel: 'icon', url: '/apple-touch-icon.png'} },
+  themeColor: '#9146FF',
 };
 
 type LayoutProps = {
@@ -16,12 +18,6 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <html lang="en" className="text-base tiny:text-tiny mobile:text-mobile normal:text-normal">
-      <head>
-        <meta name="description" content="Create podcast feeds from Twitch VODs" />
-        <meta name="theme-color" content="#9146FF" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="112x112" href="/apple-touch-icon.png" />
-      </head>
       <body className="bg-twitch">
         <main className="bg-white text-neutral-800 dark:bg-neutral-900 dark:text-white">
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MainPage from '~/components/MainPage';
 
+// Using this for static generation
 export const runtime = 'nodejs';
 
 const Page = () => <MainPage />;
