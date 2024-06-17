0b58e3cde4f09d15930f20616c96cabe7bfea6db
Trevor Sharp <sharp.trevor@me.com>
2024-06-17 14:07:12 +0000

    Remove unused env variables

--- src/env.js
+++ src/env.js
@@ -8,8 +8,6 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
-    UPSTASH_REDIS_REST_URL: z.string().url().optional(),
-    UPSTASH_REDIS_REST_TOKEN: z.string().optional(),
     TWITCH_API_CLIENT_ID: z.string(),
     TWITCH_API_SECRET: z.string(),
   },
@@ -29,8 +27,6 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     TWITCH_API_CLIENT_ID: process.env.TWITCH_API_CLIENT_ID,
     TWITCH_API_SECRET: process.env.TWITCH_API_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
