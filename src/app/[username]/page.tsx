2caa7ed1a0cc02540d91169ce015e62eb074f7bd
Trevor Sharp <sharp.trevor@me.com>
2024-07-07 17:16:59 +0000

    Cleanup

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,9 +1,7 @@
 import MainPage from "~/components/MainPage";
 
-type PageProps = {
-  params: { username: string };
-};
-
-const Page = ({ params }: PageProps) => <MainPage username={params.username} />;
+const Page = ({ params }: { params: { username: string } }) => (
+  <MainPage username={params.username} />
+);
 
 export default Page;
