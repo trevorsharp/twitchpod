fd99b8662996788833133d1d4566bdde0438700e
Trevor Sharp <sharp.trevor@me.com>
2026-05-19 15:15:18 +0000

    Clean up index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import router from "@/router";
 
-const port = parseInt(process.env["PORT"] ?? "3000");
+const port = Number(process.env["PORT"] ?? 3000);
 
-Bun.serve({ fetch: router.fetch, idleTimeout: 120, port });
+Bun.serve({
+  port,
+  fetch: router.fetch,
+  idleTimeout: 120,
+});
 
 console.log(`twitchPOD listening on port ${port}`);
